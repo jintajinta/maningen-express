d3777f858c3ccb8e7b263736c51753cef1c4e668
jintajinta <99868365+jintajinta@users.noreply.github.com>
2022-08-30 23:44:17 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex("test")
+          .insert({name: 'aaa'})
+          .then(function () {
+            res.render('index', { title: 'Express' });
+          })
 });
 
 module.exports = router;
