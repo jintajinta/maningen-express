ab97f2c08ba83584bfde6b983c1d9d39fcff98b5
jintajinta <99868365+jintajinta@users.noreply.github.com>
2022-08-30 23:31:59 +0900

    add

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    development: {
+      client: "pg",
+      client: 'pg',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    },
+  
+    staging: {
+      client: "pg",
+      client: 'pg',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    },
+  
+    production: {
+      client: 'pg',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    }
+  
+  };
